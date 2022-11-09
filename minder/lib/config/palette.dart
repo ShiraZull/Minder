6a9d438cb573ab78b3952fd7d3ef7ed01692b37d
AladinHindawi <50661339+AladinHindawi@users.noreply.github.com>
2022-11-09 01:59:11 +0100

    Login Signin Page done now working on Login Page almost complete

---
+++ minder/lib/config/palette.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class Palette {
+  static const backgroundColor = Color.fromARGB(255, 194, 229, 240);
+
+  static const loginButtonColor = Color.fromARGB(255, 67, 161, 214);
+  static const signupButtonColor = Color.fromARGB(255, 116, 194, 239);
+
+  static const minderTextColor = Color.fromARGB(255, 67, 161, 214);
+
+  static const loginTextColor = Color.fromARGB(255, 255, 255, 255);
+}
