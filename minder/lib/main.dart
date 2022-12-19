178689887f030d1efdb5cc082ed753c479313367
ShiraLinux <zulius.roolf@gmail.com>
2022-12-19 16:14:46 +0100

    add better swipe functionality

--- minder/lib/main.dart
+++ minder/lib/main.dart
@@ -5,21 +5,18 @@ import 'package:minder/views/loginPage.dart';
 import 'views/loginsignup.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Swipe Cards Demo',
+      title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Swipe Cards Demo'),
-      debugShowCheckedModeBanner: false,
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
---
+++ minder/lib/views/card_view.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+
+class CardView extends StatelessWidget {
+  const CardView({
+    Key? key,
+    this.text = "Card View",
+    this.author = "Author",
+    this.image = const Image(
+      image: AssetImage("assets/images/error.png"),
+    ),
+  }) : super(key: key);
+  final String text;
+  final Image image;
+  final String author;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Stack(
+        children: <Widget>[
+          SizedBox.expand(
+            child: Material(
+              borderRadius: BorderRadius.circular(12.0),
+              child: Container(
+                color: Colors.blueAccent,
+                child: image,
+              ),
+            ),
+          ),
+          SizedBox.expand(
+            child: Container(
+              decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                      colors: [Colors.transparent, Colors.black54],
+                      begin: Alignment.center,
+                      end: Alignment.bottomCenter)),
+            ),
+          ),
+          Align(
+            alignment: Alignment.bottomLeft,
+            child: Container(
+                padding: EdgeInsets.symmetric(vertical: 16.0, horizontal: 16.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(text,
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 20.0,
+                            fontWeight: FontWeight.w700)),
+                    Padding(padding: EdgeInsets.only(bottom: 8.0)),
+                    Text("by $author",
+                        textAlign: TextAlign.start,
+                        style: TextStyle(color: Colors.white)),
+                  ],
+                )),
+          )
+        ],
+      ),
+    );
+  }
+}
