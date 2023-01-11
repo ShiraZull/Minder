a7ad01f5193ff288392b544f7586b46c028cfdf9
ShiraLinux <zulius.roolf@gmail.com>
2023-01-11 14:06:09 +0100

    add yay and nay buttons to homepage

--- minder/lib/app/homePage.dart
+++ minder/lib/app/homePage.dart
@@ -6,7 +6,7 @@ SharedPreferences? prefs;
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
   final int something = 0;
-  
+
   Future<bool> _reset() async {
     prefs = await SharedPreferences.getInstance();
     await prefs?.remove('loggedIn');
@@ -15,32 +15,69 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Column(
-        children: [
-          const SizedBox(
-            height: 100,
-          ),
-          Card(
-            color: Colors.red,
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Container(
-                color: Colors.blue,
-                height: 400,
-                width: 300,
-              ),
+    return Column(
+      children: [
+        const SizedBox(
+          height: 50,
+        ),
+        Align(
+          alignment: Alignment.centerLeft,
+          child: TextButton(onPressed: _reset, child: const Text('RESET')),
+        ),
+        Container(
+          height: 400,
+          width: 300,
+          child: const Card(
+            clipBehavior: Clip.hardEdge,
+            color: Colors.white,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.all(Radius.circular(10)),
+            ),
+            child: Placeholder(
+              fallbackHeight: 200,
+              fallbackWidth: 200,
             ),
           ),
-          Row(
-            children: [
-              // IconButton(onPressed: () {}, icon: const Icon(Icons.add)),
-              // IconButton(onPressed: () {}, icon: const Icon(Icons.heat_pump_rounded)),
-              TextButton(onPressed: _reset, child: Text('RESET'))
-            ],
-          )
-        ],
-      ),
+        ),
+        const SizedBox(
+          height: 50,
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            ElevatedButton(
+              onPressed: null,
+              style: ButtonStyle(
+                fixedSize: MaterialStateProperty.all(const Size(80, 80)),
+                backgroundColor: MaterialStateProperty.all(Colors.red),
+                shape: MaterialStateProperty.all(
+                  const CircleBorder(),
+                ),
+              ),
+              child: const Icon(
+                Icons.close,
+                size: 40,
+                color: Colors.white,
+              ),
+            ),
+            ElevatedButton(
+              onPressed: null,
+              style: ButtonStyle(
+                fixedSize: MaterialStateProperty.all(const Size(80, 80)),
+                backgroundColor: MaterialStateProperty.all(Colors.green),
+                shape: MaterialStateProperty.all(
+                  const CircleBorder(),
+                ),
+              ),
+              child: const Icon(
+                Icons.favorite,
+                size: 40,
+                color: Colors.white,
+              ),
+            )
+          ],
+        )
+      ],
     );
   }
 }
