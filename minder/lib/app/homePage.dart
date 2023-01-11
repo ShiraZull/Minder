f55b5a30d7de3f88638a332afaa7fa4088bff559
ShiraLinux <zulius.roolf@gmail.com>
2023-01-11 14:19:11 +0100

    add bottom navigation bar visuals

--- minder/lib/app/homePage.dart
+++ minder/lib/app/homePage.dart
@@ -15,69 +15,87 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        const SizedBox(
-          height: 50,
-        ),
-        Align(
-          alignment: Alignment.centerLeft,
-          child: TextButton(onPressed: _reset, child: const Text('RESET')),
-        ),
-        Container(
-          height: 400,
-          width: 300,
-          child: const Card(
-            clipBehavior: Clip.hardEdge,
-            color: Colors.white,
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.all(Radius.circular(10)),
-            ),
-            child: Placeholder(
-              fallbackHeight: 200,
-              fallbackWidth: 200,
+    return Scaffold(
+      body: Column(
+        children: [
+          const SizedBox(
+            height: 50,
+          ),
+          Align(
+            alignment: Alignment.centerLeft,
+            child: TextButton(onPressed: _reset, child: const Text('RESET')),
+          ),
+          Container(
+            height: 400,
+            width: 300,
+            child: const Card(
+              clipBehavior: Clip.hardEdge,
+              color: Colors.white,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.all(Radius.circular(10)),
+              ),
+              child: Placeholder(
+                fallbackHeight: 200,
+                fallbackWidth: 200,
+              ),
             ),
           ),
-        ),
-        const SizedBox(
-          height: 50,
-        ),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            ElevatedButton(
-              onPressed: null,
-              style: ButtonStyle(
-                fixedSize: MaterialStateProperty.all(const Size(80, 80)),
-                backgroundColor: MaterialStateProperty.all(Colors.red),
-                shape: MaterialStateProperty.all(
-                  const CircleBorder(),
+          const SizedBox(
+            height: 50,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              ElevatedButton(
+                onPressed: null,
+                style: ButtonStyle(
+                  fixedSize: MaterialStateProperty.all(const Size(80, 80)),
+                  backgroundColor: MaterialStateProperty.all(Colors.red),
+                  shape: MaterialStateProperty.all(
+                    const CircleBorder(),
+                  ),
+                ),
+                child: const Icon(
+                  Icons.close,
+                  size: 40,
+                  color: Colors.white,
                 ),
               ),
-              child: const Icon(
-                Icons.close,
-                size: 40,
-                color: Colors.white,
-              ),
-            ),
-            ElevatedButton(
-              onPressed: null,
-              style: ButtonStyle(
-                fixedSize: MaterialStateProperty.all(const Size(80, 80)),
-                backgroundColor: MaterialStateProperty.all(Colors.green),
-                shape: MaterialStateProperty.all(
-                  const CircleBorder(),
+              ElevatedButton(
+                onPressed: null,
+                style: ButtonStyle(
+                  fixedSize: MaterialStateProperty.all(const Size(80, 80)),
+                  backgroundColor: MaterialStateProperty.all(Colors.green),
+                  shape: MaterialStateProperty.all(
+                    const CircleBorder(),
+                  ),
                 ),
-              ),
-              child: const Icon(
-                Icons.favorite,
-                size: 40,
-                color: Colors.white,
-              ),
-            )
-          ],
-        )
-      ],
+                child: const Icon(
+                  Icons.favorite,
+                  size: 40,
+                  color: Colors.white,
+                ),
+              )
+            ],
+          ),
+        ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.business),
+            label: 'Business',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.school),
+            label: 'School',
+          ),
+        ],
+      ),
     );
   }
 }
